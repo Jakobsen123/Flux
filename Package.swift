3ae1621afdf54e29dc9fb56c943570ccdaf700ca
Jakobs1 <jakobkyrkjebo@icloud.com>
2026-06-14 00:37:22 +0200

    s

--- Package.swift
+++ Package.swift
@@ -4,19 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "simplyAPI",
+    name: "Flux",
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "simplyAPI",
-            targets: ["simplyAPI"]
+            name: "Flux",
+            targets: ["Flux"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "simplyAPI"
+            name: "Flux"
         ),
 
     ]
--- Sources/Flux/Flux.swift
+++ Sources/Flux/Flux.swift
@@ -1,2 +1,30 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+public enum RequestError: Error {
+    case invalidURL
+    case httpError(statusCode: Int)
+    case decodingError(Error)
+    case networkError(Error)
+}
+
+public func sendGET<T: Decodable>(
+    url: URL,
+    as type: T.Type = T.self,
+    headers: [String: String] = [:]
+) async throws -> T {
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+    headers.forEach { request.setValue($1, forHTTPHeaderField: $0) }
+
+    let (data, response) = try await URLSession.shared.data(for: request)
+
+    if let http = response as? HTTPURLResponse, !(200...299).contains(http.statusCode) {
+        throw RequestError.httpError(statusCode: http.statusCode)
+    }
+
+    do {
+        return try JSONDecoder().decode(T.self, from: data)
+    } catch {
+        throw RequestError.decodingError(error)
+    }
+}
